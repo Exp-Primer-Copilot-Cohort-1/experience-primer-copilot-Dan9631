69601b08e70bb923dafa5a9187545108bff1fbc9
Marlon Daniel Paredes Avila <101300082+Dan9631@users.noreply.github.com>
2024-03-14 16:58:23 +0000

    Copilot second commit

---
+++ members.js
@@ -0,0 +1,7 @@
+function skillsMembers() {
+  return [
+    { name: 'John', skills: ['JavaScript', 'React', 'Node'] },
+    { name: 'Jane', skills: ['JavaScript', 'React', 'Node'] },
+    { name: 'Jack', skills: ['JavaScript', 'React', 'Node'] }
+  ]
+}
